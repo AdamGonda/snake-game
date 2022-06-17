227dc61d9b7006b0f078a346b9df8a3ed4532901
adamgonda <adamgondagyula@gmail.com>
2022-06-17 14:22:28 +0200

    remove features

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const { run } = require('loop')
 const { pipe } = require('./utils')
 const { move } = require('./move')
 const { turn } = require('./turn')
-const { seekFood } = require('./seekFood')
-const { isGameOver } = require('./isGameOver')
 
 const renderMap = { food: 'o', head: 'X', body: 'x' }
 const initialState = {
@@ -18,16 +16,14 @@ const initialState = {
     ],
     dir: 'RIGHT',
   },
-  food: { x: 12, y: 5 },
   view: { width: 20, height: 10 },
   isGameOver: false,
 }
 
 function toCommon(state) {
-  const { snake, food } = state
+  const { snake } = state
 
   return [
-    { ...food, tag: 'food' },
     ...snake.body.map((part, i) => ({
       x: part.x,
       y: part.y,
@@ -41,8 +37,6 @@ function update(state, input) {
   return pipe(state)(
     turn(input),
     move,
-    seekFood,
-    isGameOver
   )
 }
 
